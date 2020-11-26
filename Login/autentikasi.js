80da524c9f5de0cc52f12465bb5b60200e248f71
iqbalnaveliano <iqbalnaveliano@gmail.com>
2020-11-26 09:49:08 +0700

    0110120111_IqbalNaveliano_SI05
    0110120111_IqbalNaveliano_SI05

---
+++ Login/autentikasi.js
@@ -0,0 +1,18 @@
+function login(){
+    Username=document.getElementById("Username").value; 
+    Password= document.getElementById("Password").value;
+    
+   if(Username== "warmachine" && Password=="Elite123"){
+        alert("Selamat anda berhasil Login");
+        window.location="belajarjavascript.html";
+        return false;
+    }
+    
+    else{
+        alert("Username dan password salah");
+    }
+   
+  
+    
+}
+   
